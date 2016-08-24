6f154582582e5fb68aee0a08750a5ff97828e178
Jeff Puckett <jeff@jeffpuckett.com>
2016-08-24 16:09:27 -0500

    apply fix

--- test.php
+++ test.php
@@ -2,26 +2,36 @@
 
 class DataProviderDependsTest extends PHPUnit_Framework_TestCase
 {
-    public function getDataProvider(){
-      return [
-        ['real_file.txt'],
-        ['non-existent_file.txt'],
-      ];
+    protected static $failed = false;
+
+    public function getDataProvider() {
+        return [
+            ['real_file.txt'],
+            ['non-existent_file.txt'],
+        ];
     }
 
     /**
-     *  @dataProvider getDataProvider
+     * @dataProvider getDataProvider
      */
-    public function testCanBeDependedOn($data){
-      $actual = file_get_contents($data);
-      $this->assertSame('expected',$actual);
+    public function testCanBeDependedOn($data) {
+        try {
+            $actual = file_get_contents($data);
+            self::assertSame('expected', $actual);
+        } catch(Exception $e) {
+            self::$failed = true;
+            throw $e;
+        }
     }
 
     /**
-     *  @dataProvider getDataProvider
-     *  @depends testCanBeDependedOn
+     * @dataProvider getDataProvider
+     * @depends testCanBeDependedOn
      */
-    public function testCanDepend($data){
-      $this->assertTrue(false);
+    public function testCanDepend($data) {
+        if (self::$failed) {
+            self::markTestSkipped('testCanBeDependedOn failed');
+        }
+        self::assertTrue(true);
     }
 }
